13f9be1f4a5b056b48666b2673227fdc58262483
Abdikadir <54958020+aakedne@users.noreply.github.com>
2022-05-08 19:05:47 -0500

    Initial commit

---
+++ src/examples/ExtraRandom.java
@@ -0,0 +1,12 @@
+package examples;
+
+import java.util.Random;
+
+public class ExtraRandom extends Random {
+
+    public String nextletter() {
+        int letterStartAt = 97;
+        int randomInt = new Random().nextInt(26) + letterStartAt;
+                return "" + (char)randomInt;
+    }
+}
---
+++ src/examples/Randomness.java
@@ -0,0 +1,16 @@
+package examples;
+
+import java.util.Random;
+
+public class Randomness {
+    public static void main(String[] args) {
+
+        ExtraRandom random = new ExtraRandom();
+        System.out.println(random.nextInt());
+        System.out.println(random.nextBoolean());
+        System.out.println(random.nextDouble());
+
+        System.out.println(random.nextletter());
+
+    }
+}
